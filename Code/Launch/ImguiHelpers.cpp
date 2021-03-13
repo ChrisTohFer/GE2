bdf2daaea79b72b8a675de53aa55c16745c233ec
Chris <chrisrkemp@hotmail.co.uk>
2021-03-13 14:44:20 +0000

    Created a modified ge2 backend for imgui based on windows backend

--- Code/Launch/ImguiHelpers.cpp
+++ Code/Launch/ImguiHelpers.cpp
@@ -1,7 +1,7 @@
 #include "ImguiHelpers.h"
 
-#include "backend/imgui_impl_opengl2.h"
-#include "backend/imgui_impl_win32.h"
+#include "platform/imgui_backend/imgui_impl_opengl2.h"
+#include "platform/imgui_backend/imgui_impl_ge2.h"
 
 #include "SFML/Window.hpp"
 
@@ -12,47 +12,25 @@ namespace
 
 namespace ge2
 {
-    using namespace plat;
-
-    void HandleInputEvents(Input const& input)
-    {
-        //Mouse events
-        auto position = input.MousePosition();
-        auto delta = input.MouseDelta();
-        io->MouseDelta = { delta.x, delta.y };
-        io->MouseWheel = input.MouseWheelDelta();
-        for (int i = 0; i < 5; ++i) //Mouse left, right, middle, x1, x2
-        {
-            auto button = Input::Button(i + Input::Button::MouseLeft);
-
-            io->MouseClicked[i] = input.ButtonDown(button);
-            io->MouseClickedPos[i] = { position.x, position.y };
-            io->MouseDown[i] = input.ButtonHeld(button);
-        }
-    }
-
     void InitialiseImgui(Window::WindowKey const& key)
     {
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
-        ImGui_ImplWin32_Init(key.Window()->getSystemHandle());
+        ImGui_ImplGe2_Init(key);
         ImGui_ImplOpenGL2_Init();
-        ImGui_ImplOpenGL2_CreateFontsTexture();
         io = &ImGui::GetIO();
     }
     void ShutdownImgui(Window::WindowKey const&)                                          //WindowKey parameter unused, but must be provided to prove we have window access
     {
-        ImGui_ImplOpenGL2_DestroyFontsTexture();
         ImGui_ImplOpenGL2_Shutdown();
-        ImGui_ImplWin32_Shutdown();
+        ImGui_ImplGe2_Shutdown();
         ImGui::DestroyContext();
         io = nullptr;
     }
     void ImguiBeginFrame(Window::WindowKey const&, Input const& input)  //WindowKey parameter unused, but must be provided to prove we have window access
     {
-        ImGui_ImplWin32_NewFrame();
         ImGui_ImplOpenGL2_NewFrame();
-        HandleInputEvents(input);
+        ImGui_ImplGe2_NewFrame(input);
         ImGui::NewFrame();
     }
     void ImguiEndFrame(Window::WindowKey const&)                                          //WindowKey parameter unused, but must be provided to prove we have window access
--- Code/Platform/imgui_backend/imgui_impl_ge2.h
+++ Code/Platform/imgui_backend/imgui_impl_ge2.h
@@ -12,30 +12,10 @@
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
 #pragma once
+#include "Platform/Window.h"
+#include "Platform/Input.h"
 #include "imgui.h"      // IMGUI_IMPL_API
 
-IMGUI_IMPL_API bool     ImGui_ImplGe2_Init(void* hwnd);
+IMGUI_IMPL_API bool     ImGui_ImplGe2_Init(ge2::plat::Window::WindowKey const& window);
 IMGUI_IMPL_API void     ImGui_ImplGe2_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGe2_NewFrame();
-
-// Win32 message handler your application need to call.
-// - Intentionally commented out in a '#if 0' block to avoid dragging dependencies on <windows.h> from this helper.
-// - You should COPY the line below into your .cpp code to forward declare the function and then you can call it.
-#if 0
-extern IMGUI_IMPL_API LRESULT ImGui_ImplGe32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-#endif
-
-// DPI-related helpers (optional)
-// - Use to enable DPI awareness without having to create an application manifest.
-// - Your own app may already do this via a manifest or explicit calls. This is mostly useful for our examples/ apps.
-// - In theory we could call simple functions from Windows SDK such as SetProcessDPIAware(), SetProcessDpiAwareness(), etc.
-//   but most of the functions provided by Microsoft require Windows 8.1/10+ SDK at compile time and Windows 8/10+ at runtime,
-//   neither we want to require the user to have. So we dynamically select and load those functions to avoid dependencies.
-IMGUI_IMPL_API void     ImGui_ImplGe2_EnableDpiAwareness();
-IMGUI_IMPL_API float    ImGui_ImplGe2_GetDpiScaleForHwnd(void* hwnd);       // HWND hwnd
-IMGUI_IMPL_API float    ImGui_ImplGe2_GetDpiScaleForMonitor(void* monitor); // HMONITOR monitor
-
-// Transparency related helpers (optional) [experimental]
-// - Use to enable alpha compositing transparency with the desktop.
-// - Use together with e.g. clearing your framebuffer with zero-alpha.
-IMGUI_IMPL_API void     ImGui_ImplGe2_EnableAlphaCompositing(void* hwnd);   // HWND hwnd
+IMGUI_IMPL_API void     ImGui_ImplGe2_NewFrame(ge2::plat::Input const& input);
