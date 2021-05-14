bba23f605963a7181f64b373cf84b295d40e1705
Chris <chrisrkemp@hotmail.co.uk>
2021-05-14 12:36:03 +0100

    Added sprite to shapes

--- Code/Graphics/Shapes.cpp
+++ Code/Graphics/Shapes.cpp
@@ -55,6 +55,21 @@ namespace
         AttribPointer(shaderLocations::TEX_COORDS,2),
         AttribPointer(shaderLocations::NORMALS,   3)
     };
+
+    const std::vector<float> spriteVertices =
+    {
+         0.5f,  0.5f, 0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f
+    };
+    const std::vector<AttribPointer> spriteAttributes =
+    {
+        AttribPointer(shaderLocations::POSITION,  3),
+        AttribPointer(shaderLocations::TEX_COORDS,2)
+    };
 }
 
 namespace ge2::gfx
@@ -64,8 +79,14 @@ namespace ge2::gfx
         return m_singleton->m_cube;
     }
 
+    VertexArray const& Shapes::Sprite()
+    {
+        return m_singleton->m_sprite;
+    }
+
     Shapes::Shapes()
         : m_cube(cubeVertices, cubeAttributes)
+        , m_sprite(spriteVertices, spriteAttributes)
     {
         _ASSERT(!m_singleton);
         m_singleton = this;
--- Code/Graphics/Shapes.h
+++ Code/Graphics/Shapes.h
@@ -7,11 +7,13 @@ namespace ge2::gfx
     {
     public:
         static VertexArray const& Cube();
+        static VertexArray const& Sprite();
 
         Shapes();
     private:
         inline static Shapes* m_singleton = nullptr;
         const VertexArray m_cube;
+        const VertexArray m_sprite;
     };
     
 }
