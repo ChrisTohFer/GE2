e7cc9bf7d4c9e9db11c9e04a77397ab2ccaf31a1
Chris <chrisrkemp@hotmail.co.uk>
2021-06-03 21:47:52 +0100

    Added quaternion lookdirection method

--- Code/Global/Quaternion.h
+++ Code/Global/Quaternion.h
@@ -17,6 +17,8 @@ namespace ge2
         void Euler(Vector3f const&);
         Vector3f Euler() const;
 
+        void LookDirection(Vector3f const&);
+
         Matrix4x4f RotationMatrix() const;
 
         Quaternion Inverse() const;
@@ -88,6 +90,59 @@ namespace ge2
         euler.z = atan2f(-2.f * (v.x * v.y - w * v.z), 2.f * (w*w + v.y * v.y) - 1.f);
 
         return euler;
+    }
+
+    inline void Quaternion::LookDirection(Vector3f const& direction)
+    {
+        if (direction == Vector3f::Zero())
+        {
+            return;
+        }
+
+        //Set up unit vectors
+        constexpr Vector3f worldUp = Vector3f::UnitY();
+        constexpr Vector3f worldForward = Vector3f::UnitZ();
+
+        Vector3f forward = direction.Normalise();
+        Vector3f right = -CrossProduct(forward, worldUp).Normalise();
+        Vector3f up = -CrossProduct(right, forward).Normalise();
+
+        //Set quaternion values - sources:
+        //http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
+        //https://stackoverflow.com/questions/52413464/look-at-quaternion-using-up-vector/52551983#52551983
+        float trace = right.x + up.y + forward.z;
+        if (trace > 0.0f)
+        {
+            float s = 0.5 / sqrtf(trace + 1.f);
+            w = 0.25f / s;
+            v.x = (up.z - forward.y) * s;
+            v.y = (forward.x - right.z) * s;
+            v.z = (right.y - up.x) * s;
+        }
+        else if (right.x > up.y && right.x > forward.z)
+        {
+            float s = 2.0f * sqrt(1.0f + right.x - up.y - forward.z);
+            w = (up.z - forward.y) / s;
+            v.x = 0.25f * s;
+            v.y = (up.x + right.y) / s;
+            v.z = (forward.x + right.z) / s;
+        }
+        else if (up.y > forward.z)
+        {
+            float s = 2.0 * sqrt(1.0f + up.y - right.x - forward.z);
+            w = (forward.x - right.z) / s;
+            v.x = (up.x + right.y) / s;
+            v.y = 0.25f * s;
+            v.z = (forward.y + up.z) / s;
+        }
+        else
+        {
+            float s = 2.0f * sqrt(1.0f + forward.z - right.x - up.y);
+            w = (right.y - up.x) / s;
+            v.x = (forward.x + right.z) / s;
+            v.y = (forward.y + up.z) / s;
+            v.z = 0.25f * s;
+        }
     }
 
     inline Matrix4x4f Quaternion::RotationMatrix() const
